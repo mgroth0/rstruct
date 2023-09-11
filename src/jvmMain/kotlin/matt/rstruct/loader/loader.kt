969b11ba027db961c3482bafc87233abb32a8ef9
mgroth0 <mgroth49@gmail.com>
2023-09-11 12:41:26 -0400

    added src/jvmMain/kotlin/matt/rstruct/loader/loader.kt,changed src/jvmMain/kotlin/matt/rstruct/rstruct.kt

---
+++ src/jvmMain/kotlin/matt/rstruct/loader/loader.kt
@@ -0,0 +1,60 @@
+package matt.rstruct.loader
+
+import matt.file.MFile
+import matt.file.construct.mFile
+import matt.lang.classpathwork.ClassPathWorker
+import matt.prim.str.elementsToString
+import java.io.InputStream
+import java.net.URL
+
+/*because it should be explict where I am getting the resources from now that I am starting to use plugin classloaders*/
+fun systemResourceLoader() = ResourceLoader(ClassLoader.getSystemClassLoader())
+
+class ResourceLoader(vararg classLoaders: ClassLoader) : ClassPathWorker(*classLoaders) {
+
+    private fun resources(name: String): Sequence<URL> = sequence {
+        classLoaders.forEach {
+            yieldAll(it.getResources(name).asSequence())
+        }
+    }
+
+    /*It's a unix style path on all systems*/
+    fun resourceStream(name: String): InputStream? {
+        val checkedSingleURL = resourceURL(name)
+        return checkedSingleURL?.openStream()
+    }
+
+
+    /*Thing()::class.java.classLoader*/
+    /*ClassLoader.getPlatformClassLoader()*/
+    fun readResourceAsText(name: String): String? {
+        return withResourceStream(name) {
+            it?.bufferedReader()?.readText()
+        }
+    }
+
+
+    fun <R> withResourceStream(
+        name: String,
+        op: (InputStream?) -> R
+    ) = resourceStream(name).use { s ->
+        op(s)
+    }
+
+    /*It's a unix style path on all systems*/
+    fun resourceURL(name: String): URL? =
+        resources(name.replace("\\", MFile.unixSeparator)).toList().let {
+
+
+            if (it.isEmpty()) null
+            else if (it.size == 1) it.single()
+            else {
+                println("there are multiple resources with the name $name: ${it.elementsToString()}") /*because for some reasons the error message below is currently not printing*/
+                error("there are multiple resources with the name $name: ${it.elementsToString()} This will lead to bugs as it becomes ambiguous which one will be selected. Technically it is based on the classpath ordering, but that is something I want my code to be invariant to.")
+            }
+        }
+
+    fun resourceFile(path: String) = resourceURL(path)?.toURI()?.let { mFile(it) }
+
+
+}
--- src/jvmMain/kotlin/matt/rstruct/rstruct.kt
+++ src/jvmMain/kotlin/matt/rstruct/rstruct.kt
@@ -3,71 +3,31 @@ package matt.rstruct
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import matt.collect.itr.subList
-import matt.file.MFile
 import matt.file.commons.CHANGELIST_MD
 import matt.file.construct.mFile
 import matt.log.report.VersionGetterService
 import matt.model.code.mod.RelativeToKMod
 import matt.model.data.release.Version
-import matt.prim.str.elementsToString
-import java.io.InputStream
-import java.net.URL
+import matt.rstruct.loader.systemResourceLoader
 
 
+//@Deprecated("It is unclear how this will work in plugin classloaders")
 /*val appName by lazy { resourceTxt(appNameFileRelativeToResources.path) ?: "app_name_blank" }*/
 val modID by lazy {
     Json.decodeFromString<ModID>(
-        resourceTxt(modIDFileRelativeToResources.path) ?: error("${modIDFileRelativeToResources.path} not found")
+        systemResourceLoader().readResourceAsText(modIDFileRelativeToResources.path)
+            ?: error("${modIDFileRelativeToResources.path} not found")
     )
 }
 
+//@Deprecated("It is unclear how this will work in plugin classloaders")
 /*val appName by lazy { resourceTxt(appNameFileRelativeToResources.path) ?: "app_name_blank" }*/
 val extraValues by lazy {
     Json.decodeFromString<Map<String, String>>(
-        resourceTxt(valuesFileRelativeToResources.path) ?: error("${valuesFileRelativeToResources.path} not found")
+        systemResourceLoader().readResourceAsText(valuesFileRelativeToResources.path)
+            ?: error("${valuesFileRelativeToResources.path} not found")
     )
 }
-
-/*Thing()::class.java.classLoader*/
-/*ClassLoader.getPlatformClassLoader()*/
-fun resourceTxt(name: String): String? {
-    return withResourceStream(name) {
-        it?.bufferedReader()?.readText()
-    }
-}
-
-private fun systemClassLoader(): ClassLoader = ClassLoader.getSystemClassLoader()
-
-/*It's a unix style path on all systems*/
-fun resourceURL(name: String): URL? =
-    resources(name.replace("\\", MFile.unixSeparator)).toList().let {
-
-        if (it.isEmpty()) null
-        else if (it.size == 1) it.single()
-        else {
-            println("there are multiple resources with the name $name: ${it.elementsToString()}") /*because for some reasons the error message below is currently not printing*/
-            error("there are multiple resources with the name $name: ${it.elementsToString()} This will lead to bugs as it becomes ambiguous which one will be selected. Technically it is based on the classpath ordering, but that is something I want my code to be invariant to.")
-        }
-    }
-
-/*It's a unix style path on all systems*/
-fun resourceStream(name: String): InputStream? {
-    val checkedSingleURL = resourceURL(name)
-    return checkedSingleURL?.openStream()
-}
-
-fun <R> withResourceStream(
-    name: String,
-    op: (InputStream?) -> R
-) = resourceStream(name).use { s ->
-    op(s)
-}
-
-
-private fun resources(name: String): Sequence<URL> = systemClassLoader().getResources(name).asSequence()
-
-
-fun resourceFile(path: String) = resourceURL(path)?.toURI()?.let { mFile(it) }
 
 val appNameFileRelativeToResources by lazy { mFile("matt")["appname.txt"] }
 val modIDFileRelativeToResources by lazy { mFile("matt")["modID.json"] }
